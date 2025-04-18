299987e48d76d715a1acab1b2408192a1cdeea42
angus <s1712180@ed.ac.uk>
2025-04-18 21:49:35 +0100

    remove comment

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,3 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 pub fn main() !void {}
 
 const AtomicOrder = std.builtin.AtomicOrder;
